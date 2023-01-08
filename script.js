962dd1fd0633c19b9a16254da450195d981b17f0
Mohammad Aazan Arain <57153555+azan17@users.noreply.github.com>
2023-01-08 12:10:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const button = document.getElementById('button');
-button.addEventListener("click", () => {
-    const data = {
-    model: 'text-davinci-003',
-    prompt: `${input.value} ->`,
-    max_tokens: 1500,
-}
-fetch(`https://api.openai.com/v1/completions`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-         'Authorization': `Bearer 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`
-    },
-    body: JSON.stringify(data)
-})
-.then((res) => {
-    return res.json(); 
-})
-.then((data) => {
-    console.log(data);
-    const output = data.choices[0].text;
-    document.getElementById('output').innerHTML = `${output}`;
-});
-})
+document.getElementById('button').addEventListener('click', function() {
+    document.getElementById('output').innerHTML = 'GPT-3 Clone is currently in working please try again later.';
+  });
+// const button = document.getElementById('button');
+// button.addEventListener("click", () => {
+//     const data = {
+//     model: 'text-davinci-003',
+//     prompt: `${input.value} ->`,
+//     max_tokens: 1500,
+// }
+// fetch(`https://api.openai.com/v1/completions`, {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//          'Authorization': `Bearer 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`
+//     },
+//     body: JSON.stringify(data)
+// })
+// .then((res) => {
+//     return res.json(); 
+// })
+// .then((data) => {
+//     console.log(data);
+//     const output = data.choices[0].text;
+//     document.getElementById('output').innerHTML = `${output}`;
+// });
+// })
